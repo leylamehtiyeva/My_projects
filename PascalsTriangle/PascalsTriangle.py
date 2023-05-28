a2f3e8972b07b9d5953da2841f0fe85ef9edd346
Leyla Mehtiyeva <90626288+leylamehtiyeva@users.noreply.github.com>
2023-05-28 15:00:40 +0400

    Add files via upload

---
+++ PascalsTriangle/PascalsTriangle.py
@@ -0,0 +1,14 @@
+N = 0
+P = []
+
+for i in range(N+1):
+    row = [1] * (i+1)
+    for j in range(i+1):
+        if j != 0 and j != i:
+            row[j] = P[i-1][j-1] + P[i-1][j]
+
+    P.append(row)
+
+print(row)
+
+
